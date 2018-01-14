baab651866ddc22d0b3f3d62b170b5ff47e6363f
Dan Brown <jdanbrown@gmail.com>
2018-01-14 15:57:40 -0800

    setup.py: Parse install_requires from reqs.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
+from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
 setup(
     name='potoo',
     packages=find_packages(exclude=[]),
+    # Parse from reqs.txt
+    #   - https://stackoverflow.com/a/16624700/397334
+    install_requires=[str(x.req) for x in parse_requirements('requirements.txt', session='dummy')],
 )
