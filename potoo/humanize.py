f6dff2facd7e68df07ddaf9fb63d22b6bf65faf9
Dan Brown <jdanbrown@gmail.com>
2019-08-24 11:45:47 -0700

    humanize: Add kwargs

--- potoo/humanize.py
+++ potoo/humanize.py
@@ -1,15 +1,26 @@
 import humanize
 
 
-def naturalsize(size: float) -> str:
+def naturalsize_binary(size: float, **kwargs) -> str:
+    """
+    Ergonomics for humanize.naturalsize
+    - Default to binary=True
+    """
+    return naturalsize(size, **{
+        'binary': True,
+        **kwargs,
+    })
+
+
+def naturalsize(size: float, **kwargs) -> str:
     """
     Ergonomics for humanize.naturalsize
     - Fix bug: negative sizes are always formatted as bytes
     - Abbrev 'Bytes' -> 'B'
     """
     s = (
-        '-%s' % humanize.naturalsize(-size) if size < 0 else
-        humanize.naturalsize(size)
+        '-%s' % humanize.naturalsize(-size, **kwargs) if size < 0 else
+        humanize.naturalsize(size, **kwargs)
     )
     s = s.replace(' Bytes', ' B')
     return s
